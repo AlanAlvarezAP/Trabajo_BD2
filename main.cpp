0c91e3cb56440ddacdc864f0bf3da1a51176daac
AnthonyHuicho <anthony.huicho@ucsp.edu.pe>
2025-05-26 08:49:10 -0500

    cambios main cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+class node {
+public:
+	int v;
+	node* sig[2];
+	node(int as) {
+		v = as;
+		sig[0] = sig[1] = nullptr;
+	}
+};
+class arbol {
+public:
+	node* root=nullptr;
+
+};
+int main{
+	return;
+}
